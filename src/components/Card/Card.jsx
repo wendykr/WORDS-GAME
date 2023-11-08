46374a33b2332bd982dd3204d591b983f12f211b
Vendula Krajíčková <vendysacek@seznam.cz>
2023-11-08 20:17:13 +0100

    akcept change

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -28,13 +28,6 @@ export const Card = (
   // console.log('Card setupCountWord', setupCountWord);
 
   const [isMarked, setIsMarked] = useState(false);
-<<<<<<< HEAD
-  const [inputValue, setInputValue] = useState('');
-  const [answerDisplayed, setAnswerDisplayed] = useState(false);
-  const [showResult, setShowResult] = useState(false);
-  const [showCorrectAnswer, setShowCorrectAnswer] = useState(false);
-  const refInput = useRef(null);
-=======
   const [isDisplay, setIsDisplay] = useState(false);
   const [repeat, setRepeat] = useState(false);
 
@@ -68,37 +61,11 @@ export const Card = (
   const showFirstLetter = () => {
     setIsDisplay(prevState => !prevState);
   };
->>>>>>> 1533464b5c6354ffcb035d348d7b3c666b12f599
 
   const handleStarToggle = () => {
     setIsMarked(prevState => !prevState);
   };
 
-<<<<<<< HEAD
-  const showFirstLetter = () => {
-    const currentValue = word[0];
-    setInputValue(currentValue);
-    refInput.current.focus();
-  }
-
-  const answerReveal = () => {
-    setInputValue(word);
-    setAnswerDisplayed(true);
-  }
-
-  const changeWord = (event) => {
-    const myWord = event.target.value;
-    setInputValue(myWord);
-  }
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter' && inputValue.length !== 0) {
-      if (inputValue.toLowerCase() !== word.toLowerCase()) {
-        setShowResult(true);
-      } else {
-        setShowCorrectAnswer(true);
-        setInputValue(inputValue);
-=======
   const handleClick = () => {
     isTurned && setRepeat(true);
     setIsTurned(prevState => !prevState);
@@ -111,7 +78,6 @@ export const Card = (
     if (currentWordIndex > 0) {
       if (isTurned) {
         setIsTurned(false);
->>>>>>> 1533464b5c6354ffcb035d348d7b3c666b12f599
       }
       updateProgressbar(false, false);
       setCurrentWordIndex(prevValue => prevValue - 1);
@@ -119,43 +85,6 @@ export const Card = (
     }
   }
 
-<<<<<<< HEAD
-  const handleClick = () => {
-    if (inputValue.toLowerCase() !== word.toLowerCase()) {
-      setShowResult(true);
-    } else {
-      setShowCorrectAnswer(true);
-      setInputValue(inputValue);
-    }
-  }
-
-  const speak = () => {
-    let utterance = new SpeechSynthesisUtterance(word);
-    window.speechSynthesis.speak(utterance);
-  }
-
-  useEffect(() => {
-    const yourAnswerElm = document.querySelector('.your-answer');
-    const hintElm = document.querySelector('.hint');
-    const cardFootLinkElm = document.querySelector('.card__foot--link');
-    if (showResult) {
-      yourAnswerElm.classList.add('hidden');
-      hintElm.classList.add('hidden');
-      cardFootLinkElm.classList.add('hidden');
-    }
-  }, [showResult]);
-
-  useEffect(() => {
-    const yourAnswerElm = document.querySelector('.your-answer');
-    const hintElm = document.querySelector('.hint');
-    const cardFootLinkElm = document.querySelector('.card__foot--link');
-    if (showCorrectAnswer) {
-      yourAnswerElm.classList.add('hidden');
-      hintElm.classList.add('hidden');
-      cardFootLinkElm.classList.add('hidden');
-    }
-  }, [showCorrectAnswer]);
-=======
   const handleClickNext = () => {
     // console.log('%c click NEXT ', 'background:white;color:green;font-weight:bold;');
     if (currentWordIndex < (setupCountWord - 1)) {
@@ -185,7 +114,6 @@ export const Card = (
 
   //   document.addEventListener('keydown', handleKeyDown);
   // }, []);
->>>>>>> 1533464b5c6354ffcb035d348d7b3c666b12f599
 
   return (
     <div className="card">
@@ -236,52 +164,6 @@ export const Card = (
                 }
                 <FaStar className={`icon-star ${isMarked ? 'icon-star--marked' : ''}`} onClick={handleStarToggle} title="Mark icon" />
               </span>
-
-<<<<<<< HEAD
-        {(!answerDisplayed) && (
-          <p className="hint" onClick={showFirstLetter}>Hint <MdHelpCenter className="icon-hint" title="Hint icon" /></p>
-        )}
-
-        {(!answerDisplayed) && (
-          <input className="your-answer"
-                onChange={(changeWord)}
-                onKeyDown={(handleKeyDown)}
-                value={inputValue}
-                type="text"
-                ref={refInput}>
-          </input>
-        )}
-
-        {showResult && (
-          <div className="answer">
-              <p className="answer__label">Your answer</p>
-              <div className="answer__content answer--incorrect">{inputValue}</div>
-          </div>
-        )}
-
-        {showCorrectAnswer && (
-          <div className="answer">
-            <p className="answer__label">Correct answer</p>
-            <div className="answer__content answer--correct">{inputValue}</div>
-          </div>
-        )}
-
-        {(answerDisplayed || showResult) && (
-          <div className="answer">
-            <p className="answer__label">Correct answer</p>
-            <div className="answer__content answer--correct">{word}</div>
-          </div>
-        )}
-      </div>
-
-      <div className="card__foot">
-        <Button onClick={handleClick} text={(answerDisplayed || showResult || showCorrectAnswer) ? "Next" : "Check"} length={inputValue.length} inputValue={inputValue} />
-
-        {(!answerDisplayed) && (
-          <div className="card__foot--link" onClick={answerReveal}>Don&apos;t know?</div>
-        )}
-=======
-
             </div>
             <div className="container--words" onClick={handleClick} >
               <h2 className="front-word">{isCzech ? word : czWord}</h2>
@@ -295,7 +177,6 @@ export const Card = (
           handleClickPrev={handleClickPrev} 
           handleClickNext={handleClickNext} 
         />
->>>>>>> 1533464b5c6354ffcb035d348d7b3c666b12f599
       </div>
     </div>
   );
--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -2,21 +2,11 @@ import React from 'react';
 import './ProgressBar.scss';
 
 export const ProgressBar = ({ line }) => {
-<<<<<<< HEAD
     return (
       <div className="progress">
         <div className="progress__container">
           <div className="progress__line" style={{ width: `${line}%` }}></div>
         </div>
-=======
-
-  return (
-    <div className="progress">
-      <div className="progress__container">
-        <div className="progress__line" style={{ width: `${line}%` }}></div>
-        {/*line*/}
->>>>>>> 1533464b5c6354ffcb035d348d7b3c666b12f599
       </div>
-    </div>
   );
 }
