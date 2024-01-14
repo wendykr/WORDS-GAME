61cdea6fbe15faf12cf6d5c40bc24a5913efe7c5
Vendula Krajíčková <vendysacek@seznam.cz>
2024-01-14 17:03:00 +0100

    change footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export const Footer = () => {
         © 2023 <a target="_blank"  rel="noopener noreferrer" className="footer__link" href="https://github.com/wendykr/">Vendula Krajíčková</a>
       </p>
       <p className="footer__text">
-        Program project <a className="footer__link" href="https://reactgirls.com/mentoring">ReactGirls Mentoring</a>
+        Project <a className="footer__link" href="https://reactgirls.com/mentoring">ReactGirls Mentorship Program</a>
       </p>
     </footer>
   );
